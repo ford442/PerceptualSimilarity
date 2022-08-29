f5e6ac926ba43ca2c20419ad534b22f1c0a71c31
Noah Cohn <noahc42@gmail.com>
2022-08-28 19:10:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 
 import setuptools;
 from Cython.Build import cythonize;
+
+import setuptools
 with open("README.md", "r") as fh:
-    long_description = fh.read();
+    long_description = fh.read()
 setuptools.setup(
      name='lpips',  
-     version='0.1.4',
+     version='0.1',
      author="Richard Zhang",
      author_email="rizhang@adobe.com",
      description="LPIPS Similarity metric",
@@ -15,11 +17,11 @@ setuptools.setup(
      packages=['lpips'],
      package_data={'lpips': ['weights/v0.0/*.pth','weights/v0.1/*.pth']},
      include_package_data=True,
-     install_requires=["torch>=0.4.0", "torchvision>=0.2.1", "numpy>=1.14.3", "scipy>=1.0.1", "tqdm>=4.28.1"],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: BSD License",
          "Operating System :: OS Independent",
      ],
-     ext_modules = cythonize(["lpips/lpips.py"]),
+     ext_modules = cythonize(["lpips.py"]),
+
  )
