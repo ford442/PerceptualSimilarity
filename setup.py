be4588df44c9efc9e4562920641ad3298de8ace5
Noah Cohn <noahc42@gmail.com>
2022-09-05 12:05:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 
 from setuptools import setup;
 from distutils.extension import Extension;
+from Cython.Build import cythonize;
 extensions = [Extension('lpips',['lpips/lpips.py'])];
 with open("README.md", "r") as fh:
     long_description = fh.read();
@@ -21,4 +22,5 @@ setuptools.setup(
          "License :: OSI Approved :: BSD License",
          "Operating System :: OS Independent",
      ],
+     ext_modules = cythonize(extensions,nthreads=4,compiler_directives={'infer_types':True}),
  )
