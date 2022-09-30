2d811730b3e654c4c0cb44d0b16e350cea8ad9ec
Noah Cohn <noahc42@gmail.com>
2022-09-30 02:29:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,17 @@
 
 import setuptools
 from setuptools import Extension,setup; 
-
-#setup()
-#from distutils.extension import Extension;
 from Cython.Build import cythonize;
 import cython
 import pyximport; 
 pyximport.install(pyimport=True);
 extensions = [Extension('lpips',['lpips/lpips.py'])];
-#with open("README.md", "r") as fh:
-#    long_description = fh.read();
 setuptools.setup(
      name='lpips',  
      version='0.1',
      author="Richard Zhang",
      author_email="rizhang@adobe.com",
      description="LPIPS Similarity metric",
-   #  long_description=long_description,
-  #   long_description_content_type="text/markdown",
-     url="https://github.com/richzhang/PerceptualSimilarity",
      packages=['lpips'],
      package_data={'lpips': ['weights/v0.0/*.pth','weights/v0.1/*.pth']},
      include_package_data=True,
